5339c26db82918329d25e11574b5637c7d2c1ed0
Alex Durre <alexanderdurre@gmail.com>
2017-04-07 13:33:27 -0400

    added work

---
+++ Irish Poker/Irish Poker/Irish_Poker/ActionScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Irish_Poker
+{
+    class ActionScreen : GameScreen
+    {
+        public ActionScreen(Game game, SpriteBatch spriteBatch) : base(game, spriteBatch)
+        {
+        }
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+    }
+}
---
+++ Irish Poker/Irish Poker/Irish_Poker/StartScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Irish_Poker
+{
+    class StartScreen : GameScreen
+    {
+        SpriteFont spriteFont;
+
+        public StartScreen(Game game, SpriteBatch spriteBatch, SpriteFont spriteFont) : base(game, spriteBatch)
+        {
+            this.spriteFont = spriteFont;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            spriteBatch.DrawString(spriteFont, "Math Battle!", new Vector2(175, 75), Color.Black);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
